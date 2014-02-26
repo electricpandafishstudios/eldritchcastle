237be1cff1c00cc4ee85eb07e8fc41c6dd328133
sbrow <foxtrotalphalima@gmail.com>
2014-02-26 18:35:52 -0500

    updated version number

--- init.lua
+++ init.lua
@@ -24,7 +24,7 @@ long_name = "The Eldritch Castle"
 short_name = "eldritch_castle"
 author = { "ElectricPandaFish", "elp@na.org" }
 homepage = "http://te4.org/"
-version = {0,2,6}
+version = {0,2,8}
 engine = {1,1,5,"te4"}
 description = [[
 add a desciprion here later.
