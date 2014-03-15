6a6f38a4c7c3db0693bc542c2cd71fa37ec83559
sbrow <foxtrotalphalima@gmail.com>
2014-03-15 14:05:56 -0400

    added talent for firing projectile weapons, not yet implemented

--- data/talents.lua
+++ data/talents.lua
@@ -53,7 +53,29 @@ newTalent{
 		return true
 	end,
 	info = function(self, t)
-		return "Kick!"
+		return "Attack!"
+	end,
+}
+
+
+newTalent{
+	name = "Fire",
+	type = {"role/combat", 1},
+	points = 1,
+	cooldown = 0,
+	sanity = 0,
+	range = 5,
+	action = function(self, t)
+		local tg = {type="hit", range=self:getTalentRange(t)}
+		local x, y, target = self:getTarget(tg)
+		if not x or not y or not target then return nil end
+		if core.fov.distance(self.x, self.y, x, y) > 1 then return nil end
+
+		self:bumpInto(target)
+		return true
+	end,
+	info = function(self, t)
+		return "Fire!"
 	end,
 }
 
