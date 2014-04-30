edd4cd60d63596a1097c974254c09dfaafbafd39
sbrow <foxtrotalphalima@gmail.com>
2014-04-30 16:12:58 -0400

    changed level bar to better represent decay

--- class/PlayerDisplay.lua
+++ class/PlayerDisplay.lua
@@ -64,29 +64,6 @@ function _M:makeTextureBar(text, nfmt, val, max, reg, x, y, r, g, b, bar_col, ba
     return item.w, item.h, item.x, item.y
 end
 
-function _M:makePercentTextureBar(text, nfmt, val, max, flat, x, y, r, g, b, bar_col, bar_bgcol)
-    local s = self.surface_line
-    s:erase(0, 0, 0, 0)
-    s:erase(bar_bgcol.r, bar_bgcol.g, bar_bgcol.b, 255, self.bars_x, h, self.bars_w, self.font_h)
-    s:erase(bar_col.r, bar_col.g, bar_col.b, 255, self.bars_x, h, self.bars_w * val / max, self.font_h)
-
-    s:drawColorStringBlended(self.font, text, 0, 0, r, g, b, true)
-    s:drawColorStringBlended(self.font, (nfmt or "%d%%"):format((val / max) * 100), self.bars_x + 5, 0, r, g, b)
-    if flat then
-        local reg_text = (" (%d/%d)"):format(val, max)
-        local reg_txt_w = self.font:size(reg_txt)
-        s:drawColorStringBlended(self.font, reg_txt, self.bars_x + self.bars_w - reg_txt_w - 3, 0, r, g, b)
-    end
-    local item = { s:glTexture() }
-    item.x = x
-    item.y = y
-    item.w = self.w
-    item.h = self.font_h
-    self.items[#self.items+1] = item
-
-    return item.w, item.h, item.x, item.y
-end
-
 -- Displays the stats
 function _M:display()
     local player = game.player
@@ -104,9 +81,14 @@ function _M:display()
     h = h + self.font_h
 
     self:makeTextureBar("#c00000#Life:", nil, player.life, player.max_life, player.life_regen * util.bound((player.healing_factor or 1), 0, 2.5), x, h, 255, 255, 255, colors.DARK_RED, colors.VERY_DARK_RED) h = h + self.font_h
-	self:makeTextureBar("AP", nil, player.actions, player.max_actions, nil, x, h, 255, 255, 255, colors.YELLOW_GREEN, colors.OLIVE_DRAB) h = h + self.font_h
-    self:makeTextureBar("Decay", nil, player.level - 1, player.max_level - 1, nil, x, h, 255, 255, 255, colors.YELLOW_GREEN, colors.OLIVE_DRAB) h = h + self.font_h
-	self:makeTextureBar("XP", nil, player.exp, player:getExpChart(player.level + 1), nil, x, h, 255, 255, 255, colors.BLUE, colors.BLACK) h = h + self.font_h
+	self:makeTextureBar("AP:", nil, player.actions, player.max_actions, nil, x, h, 255, 255, 255, colors.BLUE, colors.DARK_BLUE) h = h + self.font_h
+    self:makeTextureBar("Decay:", nil, player.level - 1, player.max_level - 1, nil, x, h, 255, 255, 255, colors.YELLOW_GREEN, colors.OLIVE_DRAB) h = h + self.font_h
+	self:makeTextureBar("XP:", nil, player.exp, player:getExpChart(player.level + 1), nil, x, h, 255, 255, 255, colors.YELLOW_GREEN, colors.OLIVE_DRAB) h = h + self.font_h + self.font_h
+	-- s:drawStringBlended(self.font, "CON : "..(player:getCon()), w, h, 0, 255, 255, true) h = h + self.font_h
+    -- s:drawStringBlended(self.font, "ALR : "..(player:getAlr()), w, h, 255, 255, 0, true) h = h + self.font_h
+	-- s:drawStringBlended(self.font, "LCK : "..(player:getLck()), w, h, 255, 255, 255, true) h = h + self.font_h
+	-- s:drawStringBlended(self.font, "MEN : "..(player:getMen()), w, h, 255, 0, 255, true) h = h + self.font_h
+		
 	if savefile_pipe.saving then
         h = h + self.font_h
         self:makeTextureBar("Saving:", "%d%%", 100 * savefile_pipe.current_nb / savefile_pipe.total_nb, 100, nil, x, h, colors.YELLOW.r, colors.YELLOW.g, colors.YELLOW.b, 
